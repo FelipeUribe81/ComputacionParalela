379fb2998fc0f3c66fc0827d013a1ab5de1a4eb0
Felipe Uribe <felipe.uribe@correo.usa.edu.co>
2021-05-14 21:09:14 -0500

    Taller Orbitas completo

--- Simulating_Planetary_Orbits/py_simulador.py
+++ Simulating_Planetary_Orbits/py_simulador.py
@@ -115,31 +115,3 @@ def loop(bodies):
             body.py += body.vy * timestep
             #body.goto(body.px*SCALE, body.py*SCALE)
             #body.dot(3)
-
-
-def main():
-    sun = Body()
-    sun.name = 'Sun'
-    sun.mass = 1.98892 * 10**30
-    #sun.pencolor('yellow')
-
-    earth = Body()
-    earth.name = 'Earth'
-    earth.mass = 5.9742 * 10**24
-    earth.px = -1*AU
-    earth.vy = 29.783 * 1000            # 29.783 km/sec
-    #earth.pencolor('blue')
-
-    # Venus parameters taken from
-    # http://nssdc.gsfc.nasa.gov/planetary/factsheet/venusfact.html
-    venus = Body()
-    venus.name = 'Venus'
-    venus.mass = 4.8685 * 10**24
-    venus.px = 0.723 * AU
-    venus.vy = -35.02 * 1000
-    #venus.pencolor('red')
-
-    loop([sun, earth, venus])
-
-if __name__ == '__main__':
-    main()
--- Simulating_Planetary_Orbits/test.py
+++ Simulating_Planetary_Orbits/test.py
@@ -1,13 +1,15 @@
-from cy_simulador import *
+import cy_simulador as cy
+import py_simulador as py
+import time
 
-
-def main():
-    sun = Body()
+def main(simulator):
+    AU = (149.6e6 * 1000)
+    sun = simulator.Body()
     sun.name = 'Sun'
     sun.mass = 1.98892 * 10**30
     #sun.pencolor('yellow')
 
-    earth = Body()
+    earth = simulator.Body()
     earth.name = 'Earth'
     earth.mass = 5.9742 * 10**24
     earth.px = -1*AU
@@ -16,15 +18,28 @@ def main():
 
     # Venus parameters taken from
     # http://nssdc.gsfc.nasa.gov/planetary/factsheet/venusfact.html
-    venus = Body()
+    venus = simulator.Body()
     venus.name = 'Venus'
     venus.mass = 4.8685 * 10**24
     venus.px = 0.723 * AU
     venus.vy = -35.02 * 1000
     #venus.pencolor('red')
 
-    loop([sun, earth, venus])
+    simulator.loop([sun, earth, venus])
 
 if __name__ == '__main__':
-    main()    
+        initial = time.time()
+        main(py)
+        tiempoPy = time.time() - initial
+
+        initial = time.time()
+        main(cy)
+        tiempoCy = time.time() - initial
+
+        SpeedUp = round(tiempoPy/tiempoCy, 3)
+
+        print("tiempo Py: {}\n".format(tiempoPy))
+        print("tiempo Cy: {}\n".format(tiempoCy))
+        print("SpeedUp: {}\n".format(SpeedUp))
+
 
